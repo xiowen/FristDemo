0613aebb2cb06efe497c07d1eb15d87d7b2fcf03
xiaowen <xiaowen@qq.com>
2022-09-12 22:12:09 +0800

    hot-fix  commit1

--- src/com/xiaowen/Test1.java
+++ src/com/xiaowen/Test1.java
@@ -6,5 +6,6 @@ public class Test1 {
         System.out.println("hello!!!");
         System.out.println("hello!!!");
         System.out.println("hello!!!");
+        System.out.println("hello!!!");
     }
 }
