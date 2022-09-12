034f8da4ba924d814c11e55319c6a7391c69d26f
xiaowen <xiaowen@qq.com>
2022-09-12 22:15:19 +0800

    master  commit2

--- src/com/xiaowen/Test1.java
+++ src/com/xiaowen/Test1.java
@@ -7,5 +7,6 @@ public class Test1 {
         System.out.println("hello!!!");
         System.out.println("hello!!!");
         System.out.println("hello!!!");
+        System.out.println("master test!!!");
     }
 }
